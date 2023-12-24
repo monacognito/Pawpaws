64876592c535f65cc914af0d908ffb5b5fad6e15
bravadine <ygsmr.public@protonmail.com>
2023-12-24 18:38:42 +0700

    Move grooming controller - Add require database connection - Change functions' signatures - Remove commented out codes

--- controllers/grooming_controller.php
+++ controllers/grooming_controller.php
@@ -1,8 +1,8 @@
 <?php
 
-function submitGrooming($conn)
-{
+require_once(__DIR__ . "/connection.php");
 
+function submit_grooming($db_conn) {
     $member_id = trim($_POST["member_id"]);
     $price = trim($_POST["price"]);
     $date = trim($_POST["date"]);
@@ -23,17 +23,15 @@ function submitGrooming($conn)
         $error .= "ERROR: Member ID must be numerical" . nl2br("\n");
     }
     if ($error !== NULL) {
-//        echo "<script type='text/javascript'>alert('$error');</script>";
-//        echo "<meta http-equiv='refresh' content='0'>";
         return $error;
     }
 
     // Check member ID
     $query_member_id_check = "select id from members where id = ? and expired_at >= now();";
-    $result_member_id_check = safe_mysqli_query($conn, $query_member_id_check, "i", [$member_id]);
+    $result_member_id_check = safe_mysqli_query($db_conn, $query_member_id_check, "i", [$member_id]);
     if (mysqli_num_rows($result_member_id_check)) {
-        $query_new_grooming = "insert into groomings value (default, ?, default, ?, ?, ?, ?);";
-        if (safe_mysqli_query($conn, $query_new_grooming, "issii", [$member_id, $date, $time, $price, 0], false)) {
+        $query_new_grooming = "insert into grooming value (default, ?, default, ?, ?, ?, ?);";
+        if (safe_mysqli_query($db_conn, $query_new_grooming, "issii", [$member_id, $date, $time, $price, 0], false)) {
             $result = "Submission successful for ID " . $member_id;
         } else {
             $result = "ERROR: Unknown database error occurred";
@@ -42,30 +40,24 @@ function submitGrooming($conn)
         $result = "ERROR: Invalid ID. Make sure membership status is active";
     }
 
-//    echo "<script type='text/javascript'>alert('$result');</script>";
-//    echo "<meta http-equiv='refresh' content='0'>";
     return $result;
 }
 
-function payGrooming($id_pay, $conn_pay)
-{
-    $query_extend_member = "update groomings set is_paid = true where id = ?;";
-    if (safe_mysqli_query($conn_pay, $query_extend_member, "i", [$id_pay], false)) {
+function pay_grooming($db_conn, $member_id) {
+    $query_extend_member = "update grooming set is_paid = true where id = ?;";
+    if (safe_mysqli_query($db_conn, $query_extend_member, "i", [$member_id], false)) {
         $result = "Payment successful";
         echo "<meta http-equiv='refresh' content='0'>";
     } else {
         $result = "Cannot pay";
     }
 
-//    echo "<script type='text/javascript'>alert('$result');</script>";
-//    echo "<meta http-equiv='refresh' content='0'>";
     return $result;
 }
 
-function deleteGrooming($id_delete, $conn_delete)
-{
-    $query_delete_grooming = "delete from groomings where id = ?;";
-    if (safe_mysqli_query($conn_delete, $query_delete_grooming, "i", [$id_delete], false)) {
+function delete_grooming($db_conn, $member_id) {
+    $query_delete_grooming = "delete from grooming where id = ?;";
+    if (safe_mysqli_query($db_conn, $query_delete_grooming, "i", [$member_id], false)) {
         $result = "Deletion successful";
     } else {
         $result = "cannot delete";
